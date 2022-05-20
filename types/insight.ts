f2718f11decfda9a1af7945cb25fb25a7ce1f9d1
Szentes Szabolcs <szentes.szabolcs@sonrisa.hu>
2022-05-20 18:56:36 +0200

    Parsing the line to Influencer type

---
+++ types/insight.ts
@@ -0,0 +1,11 @@
+export interface Influencer {
+  instaUserName: string
+  instagramName: string
+  category1: string
+  category2: string
+  followers: number
+  country: string
+  authenticEngagement: number
+  engagementAvg: number
+}
+
--- util/InsigthUtil.ts
+++ util/InsigthUtil.ts
@@ -1,6 +1,7 @@
 import { createInterface } from "readline";
 import { createReadStream } from "fs";
 import { once } from "events";
+import { Influencer } from '../types/insight'
 
 export async function createInsightFromInfluencerFile() {
   try {
@@ -10,7 +11,9 @@ export async function createInsightFromInfluencerFile() {
     });
 
     readline.on('line', (line) => {
+      const influencer = parseLine(line)
       console.log(`Line from file: ${line}`);
+      console.log(`Influencer from line: ${JSON.stringify(influencer)}`);
     });
 
     await once(readline, 'close');
@@ -18,5 +21,30 @@ export async function createInsightFromInfluencerFile() {
     console.log("processing finished")
   } catch (error) {
     console.error(error);
+  }
+}
+
+function parseLine(line: string): Influencer {
+  const columns = line.split(",");
+  return {
+    instaUserName: columns[0],
+    instagramName: columns[1],
+    category1: columns[2],
+    category2: columns[3],
+    followers: parseNumber(columns[4]),
+    country: columns[5],
+    authenticEngagement: parseNumber(columns[6]),
+    engagementAvg: parseNumber(columns[7])  
+  }
+}
+
+function parseNumber(encodedNumber: string): number {
+  switch (encodedNumber.charAt(encodedNumber.length - 1)) {
+    case 'K':
+      return parseFloat(encodedNumber) * 1000;
+    case 'M':
+      return parseFloat(encodedNumber) * 1000000;
+    default:
+      return parseFloat(encodedNumber);
   }
 }
