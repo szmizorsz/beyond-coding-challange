e7c36b98bfeca947ceb213f730c30f13e1de23b2
Szentes Szabolcs <szentes.szabolcs@sonrisa.hu>
2022-05-20 20:16:08 +0200

    Displaying the Insight on the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Insight, InsightResponse } from '../types/insight'
 
 const Home: NextPage = () => {
+
+  const [insight, setInsight] = useState<Insight>()
+
+  useEffect(() => {
+    async function init() {
+      const response = await fetch('/api/insight');
+      const responseJson = (await response.json()) as InsightResponse
+      setInsight(responseJson.insight)
+    }
+    init()
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,58 +27,28 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+      {insight && 
+      <p className={styles.description}>
+        <h3>Top influencers per category by followers</h3>
+        {Object.keys(insight.topInfluencersPerCategoryByFollowers).map((category) => {
+          return <div key={category}>
+            Category: {category},
+            Influencer: {insight.topInfluencersPerCategoryByFollowers[category].name},
+            Followers: {insight.topInfluencersPerCategoryByFollowers[category].value}
+          </div>
+        })}
+        <h3>Top influencers per country by engagement avg</h3>
+        {Object.keys(insight.topInfluencersPerCountryByEngagementAvg).map((country) => {
+          return <div key={country}>
+            Country: {country},
+            Influencer: {insight.topInfluencersPerCountryByEngagementAvg[country].name},
+            Followers: {insight.topInfluencersPerCountryByEngagementAvg[country].value}
+          </div>
+        })}
+      </p>}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
